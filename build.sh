d7f4b2eaa28afaa4b6a5cea211b241a5205c614f
Javier <javier@jpacheco.xyz>
2024-05-28 18:24:51 -0500

    update web

--- build.sh
+++ build.sh
@@ -5,6 +5,6 @@
 # Project: My web page build script
 
 emacs -Q --script build-site.el
-git add .
-git commit -m "commit $(date '+%Y-%m-%d | %r')"
-git push && echo "website updated and pushed to github" || echo "something is wrong..."
+# git add .
+# git commit -m "commit $(date '+%Y-%m-%d | %r')"
+# git push && echo "website updated and pushed to github" || echo "something is wrong..."
